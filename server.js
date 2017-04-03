327c9f3003f9da76641bc7125c4cdb1fb23fbf9c
nnattawat <armmer1@gmail.com>
2017-04-03 18:31:45 +1000

    Init commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+
+// Create an app
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(3001, function() {
+  console.log('express is running');
+});
