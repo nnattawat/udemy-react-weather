51b9fba6fd6bac476d90cbdae86405d04f438e0a
nnattawat <armmer1@gmail.com>
2017-04-12 14:24:22 +1000

    Prep repo for heroku

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-var express = require('express');
+'use strict'
 
-
+let express = require('express');
 // Create an app
-var app = express();
+let app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use((req, res, next) => {
+  // forward https to http so that it work with free open weather
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3001, function() {
-  console.log('express is running');
+app.listen(PORT, function() {
+  console.log('express is running on port: ' + PORT);
 });
